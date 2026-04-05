2e02fa01fcdd58a7e48c83f3fb3adb89df06fbe1
Ummu Salma M. H. <ummusalmahasmy@gmail.com>
2026-04-05 19:10:55 +0530

    feat: added routes for update, delete and get a user

--- routes/users.js
+++ routes/users.js
@@ -24,4 +24,56 @@ router.get('/', async (req, res) => {
     }
 });
 
+// READ ONE
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// UPDATE
+router.patch('/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true } // return updated data
+        );
+
+        if (!updatedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.send(updatedUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+        if (!deletedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.send({
+            message: "User deleted successfully",
+            user: deletedUser
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 const e = require('express');
-require('dotenv').config();
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect(env.MONGO_URI)
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
