2124ffecbd9282c749e0b298357ae77f468af0c7
Ummu Salma M. H. <ummusalmahasmy@gmail.com>
2026-04-05 18:57:26 +0530

    fix: mingi credential expose fixed

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
+const e = require('express');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://sally07152002:sally0715202@cluster0-shard-00-00.rnnza.mongodb.net:27017,cluster0-shard-00-01.rnnza.mongodb.net:27017,cluster0-shard-00-02.rnnza.mongodb.net:27017/labDB?ssl=true&replicaSet=atlas-m9yeij-shard-0&authSource=admin&appName=Cluster0')
+mongoose.connect(env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
